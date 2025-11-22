777fa6c01be047eb95522baba3c605522626b7d3
Erik Taylor <etaylor@hhsterling.com>
2025-11-22 18:09:42 -0500

    Fixed rick animation config.

--- boards/shields/nice_view_gem/widgets/rick_animation.c
+++ boards/shields/nice_view_gem/widgets/rick_animation.c
@@ -31,7 +31,7 @@ void draw_rick_animation(lv_obj_t *canvas) {
     lv_obj_center(art);
 
     lv_animimg_set_src(art, (const void **)anim_imgs, 16);
-    lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
+    lv_animimg_set_duration(art, CONFIG_NICE_VIEW_RICK_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 #else
