fe6bcb38c9da13027047938bc6774f2237cea9e8
Erik Taylor <etaylor@hhsterling.com>
2025-11-23 00:12:27 -0500

    Trying background color on widget instead.

--- boards/shields/nice_view_gem/widgets/screen_peripheral.c
+++ boards/shields/nice_view_gem/widgets/screen_peripheral.c
@@ -105,13 +105,13 @@ ZMK_SUBSCRIPTION(widget_peripheral_status, zmk_split_peripheral_status_changed);
 
 int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
+    lv_obj_set_style_bg_color(widget->obj, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(widget->obj, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_size(widget->obj, SCREEN_HEIGHT, SCREEN_WIDTH);
 
     lv_obj_t *top = lv_canvas_create(widget->obj);
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
-    lv_obj_set_style_bg_color(top, lv_color_black(), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(top, LV_OPA_COVER, LV_PART_MAIN);
 
     draw_gem_animation(widget->obj);
 
