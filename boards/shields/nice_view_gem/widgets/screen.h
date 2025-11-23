d7009b28036f520763138417777d5f3dfe4bfe8c
Erik Taylor <etaylor@hhsterling.com>
2025-11-23 16:10:58 -0500

    Fixed screen.h

--- boards/shields/nice_view_gem/widgets/screen.h
+++ boards/shields/nice_view_gem/widgets/screen.h
@@ -4,14 +4,12 @@
 #include <zephyr/kernel.h>
 #include "util.h"
 
-#define CANVAS_WIDTH  SCREEN_WIDTH
-#define CANVAS_HEIGHT SCREEN_HEIGHT
-#define CANVAS_PIXELS (CANVAS_WIDTH * CANVAS_HEIGHT)
-
 struct zmk_widget_screen {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_color_t cbuf[CANVAS_PIXELS];
+    lv_color_t cbuf[BUFFER_SIZE_TOP * BUFFER_SIZE];
+    lv_color_t cbuf2[BUFFER_SIZE_MIDDLE * BUFFER_SIZE];
+    // lv_color_t cbuf3[BUFFER_SIZE_BOTTOM * BUFFER_SIZE];
     struct status_state state;
 };
 
